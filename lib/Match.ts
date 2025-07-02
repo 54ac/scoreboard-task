74f1b3eb6e4191fa7491f1d8a0ede0428c895678
54ac <me@54ac.ovh>
2025-07-02 21:20:29 +0200

    Add start match and get summary functionality

--- lib/Match.ts
+++ lib/Match.ts
@@ -1,1 +1,12 @@
-export class Match {}
+export class Match {
+	constructor(
+		public homeTeam: string,
+		public awayTeam: string,
+		public homeScore: number = 0,
+		public awayScore: number = 0
+	) {}
+
+	toString(): string {
+		return `${this.homeTeam} ${this.homeScore} - ${this.awayScore} ${this.awayTeam}`;
+	}
+}
--- lib/Scoreboard.ts
+++ lib/Scoreboard.ts
@@ -1,1 +1,14 @@
-export class Scoreboard {}
+import { Match } from "./Match";
+
+export class Scoreboard {
+	private matches: Match[] = [];
+
+	startMatch(home: string, away: string) {
+		const match = new Match(home, away);
+		this.matches.push(match);
+	}
+
+	getSummary(): string[] {
+		return this.matches.map((m) => m.toString());
+	}
+}
