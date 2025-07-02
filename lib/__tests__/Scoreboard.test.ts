f3a64b7f9f9663ff4019e344019b1dbeffb8d1cd
54ac <me@54ac.ovh>
2025-07-02 21:40:51 +0200

    Add test for updating score

--- lib/__tests__/Scoreboard.test.ts
+++ lib/__tests__/Scoreboard.test.ts
@@ -1,11 +1,23 @@
-import { describe, it, expect } from "vitest";
+import { describe, it, expect, beforeEach } from "vitest";
 import { Scoreboard } from "../Scoreboard";
 
 describe("Scoreboard", () => {
+	let board: Scoreboard;
+
+	beforeEach(() => {
+		board = new Scoreboard();
+	});
+
 	it("starts a match (score 0-0)", () => {
-		const board = new Scoreboard();
 		board.startMatch("Poland", "Moldova");
 		const summary = board.getSummary();
 		expect(summary).toEqual(["Poland 0 - 0 Moldova"]);
+	});
+
+	it("updates the score", () => {
+		board.startMatch("Poland", "Moldova");
+		board.updateMatch("Poland", "Moldova", 2, 3);
+		const summary = board.getSummary();
+		expect(summary).toEqual(["Poland 2 - 3 Moldova"]);
 	});
 });
