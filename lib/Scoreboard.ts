36aac27b460dad816e0ea827f8169215c5dc0f0f
54ac <me@54ac.ovh>
2025-07-03 19:43:42 +0200

    Add error throwing functionality

--- lib/Scoreboard.ts
+++ lib/Scoreboard.ts
@@ -3,21 +3,34 @@ import { v4 as uuidv4 } from "uuid";
 
 export class Scoreboard {
 	private matches: Match[] = [];
+	private teams: string[] = [];
 
 	startMatch(homeTeam: string, awayTeam: string) {
+		if (this.teams.includes(homeTeam) || this.teams.includes(awayTeam))
+			throw new Error("Team already playing");
+
 		const id = uuidv4();
 		const match = new Match({ id, homeTeam, awayTeam });
 		this.matches.push(match);
+		this.teams.push(homeTeam, awayTeam);
 		return id;
 	}
 
 	updateMatch(id: string, homeScore: number, awayScore: number) {
 		const match = this.matches.find((m) => m.id === id);
-		if (match) match.update(homeScore, awayScore);
+		if (!match) throw new Error("Match not found");
+
+		match.update(homeScore, awayScore);
 	}
 
 	finishMatch(id: string) {
+		const match = this.matches.find((m) => m.id === id);
+		if (!match) throw new Error("Match not found");
+
 		this.matches = this.matches.filter((m) => m.id !== id);
+		this.teams = this.teams.filter(
+			(t) => t !== match.homeTeam && t !== match.awayTeam
+		);
 	}
 
 	getSummary(): string[] {
